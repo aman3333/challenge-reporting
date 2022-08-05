b9e83aa0db0baa469ac40d9b83f2f51b4db7f964
aman jain <amanjain@amans-MacBook-Pro.local>
2022-08-05 13:30:10 +0530

    first commit of solution

--- api.js
+++ api.js
@@ -1,30 +1,139 @@
-const knex = require('./db')
+const knex = require("./db");
+const request = require("request");
+const yj = require("yieldable-json");
+let studentgradesData = [];
+request(
+  "https://outlier-coding-test-data.netlify.app/grades.json",
+  function (error, response, body) {
+    yj.parseAsync(body, null, 32, (err, data) => {
+      if (!err) studentgradesData = data;
+    });
+
+  }
+);
+
+async function getHealth(req, res, next) {
+  try {
+    await knex("students").first();
+    res.json({ success: true });
+  } catch (e) {
+    console.log(e);
+    res.status(500).end();
+  }
+}
+
+async function getStudent(req, res, next) {
+  try {
+    let data = await knex("students").where({
+      id: req.params.id,
+    });
+    if (data.length) res.json({ studentData: data[0] });
+    else res.status(404).send("Student not found");
+  } catch (e) {
+    console.log(e);
+    res.status(500).end();
+  }
+}
+
+async function getStudentGradesReport(req, res, next) {
+  let studentCourses = [];
+  const filterCourseGrades = (init, length, cb) => {
+    setImmediate(function () {
+      if (init >= length) {
+        cb();
+
+        return;
+      }
+
+      for (let i = init; i < init + 1000 && i < length; i++) {
+        if (studentgradesData[i].id == req.params.id) {
+          studentCourses.push(studentgradesData[i]);
+        }
+      }
+
+      filterCourseGrades(init + 1000, length, cb);
+    });
+  };
+  try {
+    let data = await knex("students").where({
+      id: req.params.id,
+    });
+    if (data.length) {
+      filterCourseGrades(0, studentgradesData.length, () => {
+        res
+          .status(200)
+          .json({ studentGrades: studentCourses, studentDetails: data[0] });
+      });
+    } else {
+      res.status(404).send("Student not found");
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).end();
+  }
+}
+
+async function getCourseGradesReport(req, res, next) {
+  let courseObject = {};
+  const itterateCourseGrades = (init, length, cb) => {
+    setImmediate(function () {
+      if (init >= length) {
+        cb();
+
+        return;
+      }
+
+      for (let i = init; i < init + 1000 && i < length; i++) {
+        let course = studentgradesData[i].course;
+        let grade = studentgradesData[i].grade;
+        if (courseObject[course]) {
+          courseObject[course].numofstudents =
+            courseObject[course].numofstudents + 1;
+          courseObject[course].totalMarks += grade;
+          (courseObject[course].heighest =
+            courseObject[course].heighest > grade
+              ? courseObject[course].heighest
+              : grade),
+            (courseObject[course].lowest =
+              courseObject[course].lowest < grade
+                ? courseObject[course].lowest
+                : grade);
+        } else {
+          courseObject[course] = {
+            course: course,
+            numofstudents: 1,
+            totalMarks: grade,
+            heighest: grade,
+            lowest: grade,
+          };
+        }
+      }
+
+      itterateCourseGrades(init + 1000, length, cb);
+    });
+  };
+
+  try {
+    itterateCourseGrades(0, studentgradesData.length, () => {
+      let formatedResponse = Object.values(courseObject).map((item) => {
+        return {
+          course: item.course,
+          avgGrade: (item.totalMarks / item.numofstudents).toFixed(2),
+          heighest: item.heighest,
+          lowest: item.lowest,
+        };
+      });
+      res.status(200).json({ courseObject: formatedResponse });
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).end();
+  }
+}
 
 module.exports = {
   getHealth,
   getStudent,
   getStudentGradesReport,
-  getCourseGradesReport
-}
-
-async function getHealth (req, res, next) {
-  try {
-    await knex('students').first()
-    res.json({ success: true })
-  } catch (e) {
-    console.log(e)
-    res.status(500).end()
-  }
-}
-
-async function getStudent (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
-}
-
-async function getStudentGradesReport (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
-}
-
-async function getCourseGradesReport (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
-}
+  getCourseGradesReport,
+};
--- test.js
+++ test.js
@@ -1,27 +1,93 @@
-const tape = require('tape')
-const jsonist = require('jsonist')
+const tape = require("tape");
+const jsonist = require("jsonist");
 
-const port = (process.env.PORT = process.env.PORT || require('get-port-sync')())
-const endpoint = `http://localhost:${port}`
+const port = (process.env.PORT =
+  process.env.PORT || require("get-port-sync")());
+const endpoint = `http://localhost:${port}`;
 
-const server = require('./server')
+const server = require("./server");
 
-tape('health', async function (t) {
-  const url = `${endpoint}/health`
+tape("health", async function (t) {
+  const url = `${endpoint}/health`;
   try {
-    const { data, response } = await jsonist.get(url)
+    const { data, response } = await jsonist.get(url);
     if (response.statusCode !== 200) {
-      throw new Error('Error connecting to sqlite database; did you initialize it by running `npm run init-db`?')
+      throw new Error(
+        "Error connecting to sqlite database; did you initialize it by running `npm run init-db`?"
+      );
     }
-    t.ok(data.success, 'should have successful healthcheck')
-    t.end()
+    t.ok(data.success, "should have successful healthcheck");
+    t.end();
   } catch (e) {
-    t.error(e)
+    t.error(e);
+  }
+});
+
+tape("getStudent", async function (t) {
+  const url = `${endpoint}/student/2`;
+  try {
+    const { data, response } = await jsonist.get(url);
+    if (response.statusCode !== 200) {
+      throw new Error(
+        "Error in retriving student data from api call , please check db connection or api code?"
+      );
+    }
+    if (response.statusCode == 200) {
+      t.equal(data.studentData.id, 2, "should have student id 2");
+    }
+    t.pass("getStudent works fine");
+    t.end();
+  } catch (e) {
+    t.error(e);
+  }
+});
+
+tape("getStudentGradesReport", async function (t) {
+  const url = `${endpoint}/student/2/grades`;
+  try {
+    const { data, response } = await jsonist.get(url);
+    if (response.statusCode !== 200) {
+      throw new Error(
+        "Error in retriving student grades data from api call , please check db connection or api code?"
+      );
+    }
+    if (response.statusCode == 200) {
+      t.equal(data.studentDetails.id, 2, "should have 2 grades");
+      data.studentGrades.forEach(element => {
+        t.equal(element.id.id, 2, "should have 2 grades");
+        
+      });
+   
+
+    }
+    t.pass("getStudentGradesReport works fine");
+    t.end();
+  } catch (e) {
+    t.error(e);
   }
 })
 
-tape('cleanup', function (t) {
-  server.closeDB()
-  server.close()
-  t.end()
+tape("getCourseGradesReport", async function (t) {
+  const url = `${endpoint}/course/all/grades`;
+  try {
+    const { data, response } = await jsonist.get(url);
+    if (response.statusCode !== 200) {
+      throw new Error(
+        "Error in retriving course grades data from api call , please check db connection or api code?"
+      );
+    }
+    if (response.statusCode == 200) {
+      t.pass("getCourseGradesReport works fine");
+    }
+   
+    t.end();
+  } catch (e) {
+    t.error(e);
+  }
 })
+
+tape("cleanup", function (t) {
+  server.closeDB();
+  server.close();
+  t.end();
+});
